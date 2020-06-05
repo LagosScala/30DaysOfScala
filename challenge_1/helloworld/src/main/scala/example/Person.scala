590a2a0428fc2300fbbcabba1640482673b06356
oluwasayo <oluwasayoakinkunmi@gmail.com>
2020-06-05 12:29:42 +0100

    Assignment 4

--- challenge_1/helloworld/src/main/scala/example/Person.scala
+++ challenge_1/helloworld/src/main/scala/example/Person.scala
@@ -2,10 +2,10 @@ package example
 
 object Person extends App {
     case class Person (name: String, age: Int, bvn: Long)
-    
+
     val p = Person("sayo", 99, 453423455)
 
-    println(Person.unapply(p).isInstanceOf[Some])
+    println(Person.unapply(p).isInstanceOf[Option[_]])
 
 }
 
