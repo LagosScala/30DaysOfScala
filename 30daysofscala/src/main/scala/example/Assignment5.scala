4575c5e5e3166b33a3c1290f9897a0413aef11e3
mmekut-mfon.edet <mmekut-mfon.edet@interswitchgroup.com>
2020-06-17 11:04:24 +0100

    Assignment 5 done

---
+++ 30daysofscala/src/main/scala/example/Assignment5.scala
@@ -0,0 +1,35 @@
+object Point {
+
+  def stringToInt(x: String): Option[Int] = {
+    try {
+      Some(x.toInt)
+    } catch {
+      case e: Exception => None
+    }
+  }
+
+  def apply(sx: String, sy: String): Point = {
+    val x = stringToInt(sx)
+    val y = stringToInt(sy)
+    (x, y) match {
+
+      case (Some(x), Some(y)) => Point(x, y)
+      case (_, _)             => Point(0, 0)
+    }
+  }
+
+}
+case class Point(x: Int, y: Int)
+
+sealed trait Lang
+case object Scala extends Lang
+case object Java extends Lang
+case object Python extends Lang
+
+object SearchIfObjectExtendLang extends App {
+  def convertToString(lan: Lang): String = lan match {
+
+    case Scala | Java | Python => s"Lang language is good"
+    case _                     => "Not within the language Lang"
+  }
+}
